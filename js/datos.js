4fd1f4a2ee723cfb5f47f867279868a30f6693c1
cesurpagaelwifi <enrique.s258094@cesurformacion.com>
2024-10-15 13:42:55 +0200

    varios cambios

---
+++ js/datos.js
@@ -0,0 +1,4 @@
+let fotos = ["tornillo.png", "martillo.png", "taladro.png", "llaveinglesa.png", "destornillador.png", "sierra.png", "cinta-metrica.png", "clavo.png", "alicate.png", "escoba.png"];
+let productos = ["Tornillo 5mm", "Martillo de acero", "Taladro eléctrico", "Llave inglesa", "Destornillador estrella", "Sierra manual", "Cinta métrica", "Clavo 3cm", "Alicate multiuso", "Escoba industrial"];
+let pcostos = [0.50, 8.00, 50.00, 10.00, 2.50, 7.00, 3.00, 0.05, 5.00, 4.50];
+let beneficios = [0.30, 4.00, 20.00, 5.00, 1.50, 3.00, 1.50, 0.02, 2.00, 1.50];
